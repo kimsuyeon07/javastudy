b51d85213621995060426148e0fe13d1dd4fa20d
kimsuyeon <ksy724@naver.com>
2021-03-16 15:49:49 +0900

    [03]main=>updatetest

--- workspace/study/src/control_Quiz/loop_Quiz02.java
+++ workspace/study/src/control_Quiz/loop_Quiz02.java
@@ -5,28 +5,29 @@ import java.util.Scanner;
 public class loop_Quiz02 {
 
 	public static void main(String[] args) {
-
 		// 영화 평점을 입력받아서
-		// ★을 출력
-		// 평점은 1~5 사이의 정수이고, 잘못된 평점은 다시 입력 받습니다.
-		
-		Scanner sc = new Scanner(System.in);
-		
-//		int grade = 0;
-		
-		
-		System.out.print("평점을 입력하세요 : ");
-		int grade = sc.nextInt();
-		
-		for(int n = 0 ; n < 6 ; n++) {
-			grade++;
-		}
-		
-//		switch (grade) {
-//		case 1: 2: 3: 4: 5: ★; break;
-//		default: System.out.println("평점이 없습니다."); 
-//		
-//		}
+				// ★을 출력
+				// 평점은 1~5 사이의 정수이고, 잘못된 평점은 다시 입력 받습니다.
+				
+				Scanner sc = new Scanner(System.in);
+				int grade = 0;
+				do {
+					if (grade >= 1) {System.out.println("다시 입력하세요.");}
+					System.out.print("평점을 입력하세요 : ");
+					grade = sc.nextInt();
+				} while (1 > grade || grade > 5);  // grade가 1보다 작거나 5보다 크면 실행하겠다.
+					
+				String stars = ""; // 문자열 stars의 초기화  [ 빈 문자열 ("")은 문자열 연결 연산 (+)의 경우에 초기화로 사용한다. ]
+				                   // null : 저장된 문자열이 없다는 의미. (일반적으로는 사용이 맞지만, ""을 사용해야하는 경우도 확인!)
+				for (int n = 0 ; n < grade ; n++) {
+					// grade만큼 반복문을 짜려고 한다.
+					stars += "★ " ;
+				}
+				
+				System.out.println("평점 : " + grade + " ( " + stars + " )");
+				
+				
+				sc.close();
 		
 		
 		
