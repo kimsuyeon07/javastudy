f5fdd2d0f1b8fbfed5b8a8abffffe7f4e3d3f064
kimsuyeon <ksy724@naver.com>
2021-03-16 15:44:06 +0900

    [03]update test

--- workspace/study/src/control_Quiz/loop_Quiz02.java
+++ workspace/study/src/control_Quiz/loop_Quiz02.java
@@ -12,12 +12,21 @@ public class loop_Quiz02 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int grade = 0;
-		
-		switch (grade) {
+//		int grade = 0;
 		
 		
+		System.out.print("평점을 입력하세요 : ");
+		int grade = sc.nextInt();
+		
+		for(int n = 0 ; n < 6 ; n++) {
+			grade++;
 		}
+		
+//		switch (grade) {
+//		case 1: 2: 3: 4: 5: ★; break;
+//		default: System.out.println("평점이 없습니다."); 
+//		
+//		}
 		
 		
 		
