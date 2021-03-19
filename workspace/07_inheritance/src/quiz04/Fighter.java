f1a0207b8e7713f5269199424c793e23a34f2bc7
kimsuyeon <ksy724@naver.com>
2021-03-19 09:45:41 +0900

    update 2021-03-19

--- workspace/07_inheritance/src/quiz04/Fighter.java
+++ workspace/07_inheritance/src/quiz04/Fighter.java
@@ -19,6 +19,49 @@ public abstract class Fighter {
 	
 	// method
 	public abstract void attack(Fighter fighter);  // 내가 누구에게 공격한다.
+
+	
+	// method : Getter and Setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isAlive() {
+		return isAlive;
+	}
+	public void setAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+	}
+	public int getEnergy() {
+		return energy;
+	}
+	public void setEnergy(int energy) {
+		// energy가 음수로 떨어지면 0을 주겠다.  
+		this.energy = this.energy < 0 ? 0 : this.energy;
+		// energy가 0보다 커야 살아있다고 본다.
+		setAlive(this.energy > 0);
+	}
+	public int getPower() {
+		return power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	// method : info()
+	public void info() {
+		System.out.println("[ 이름 : " + name + ", 에너지  : " + energy + ", 공격력 : " + power + " ]");
+	}
+
+
+
+
+
+
+	
+	
 	
 	
 	
--- workspace/07_inheritance/src/quiz04/KoreanFighter.java
+++ workspace/07_inheritance/src/quiz04/KoreanFighter.java
@@ -7,17 +7,38 @@ public class KoreanFighter extends Fighter {
 	
 	// constructor
 	public KoreanFighter(String name) {
+		super(name, (int)(Math.random()*100)+1, (int)(Math.random()*10)+1);
+	}
 		
-		int energy = (int)(Math.random()*100) + 1;
-		int power = (int)(Math.random()*10) + 1;
 		
-		super(name, energy, power);
+	// method
+	// attack() : 20% 확률로 한 방에 KO 시킬 수 있다.
+	@Override
+	public void attack(Fighter fighter) {  
+		// * Golbal, Korean 모두 담을 수 있는 Fighter로 지정 *
+		// 공격(power)하면 상대방fighter의 에너지(energy)에 데미지를 입힌다.
+		// 나 : this  |  상대방 : fighter
+		
+		// 상대편의 에너지를 20%확률로 제로로 만들 수 있다.
+		if(Math.random() < 0.2) {
+			fighter.setEnergy(0);
+			System.out.println(fighter.getName() + " KO!");
+		  // 아니면 내 power만큼 상대편 energy가 줄어든다.
+		} else {
+			fighter.setEnergy(fighter.getEnergy() - this.getPower());
+			System.out.println("공격 : " + this.getName() + " [ " + fighter.getName() + " 남은 에너지 : " + fighter.getEnergy() + " ]");
+		}
+		
+		// 살아있다.
+		
+		
+	
 		
 	}
+		
 	
 	
-	// method
-	// attack() : 20% 확률로 한 방에 KO 시킬 수 있다.  
+	
 	
 	
 	
